95e27dd2df1301df7853278b3a8bfc71c3fcb6c5
Jex Pearce <jex@jajajeev.com>
2025-06-13 15:37:55 +0500

    e

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jex-portfolio/',
   build: {
-    minify: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
+    outDir: 'dist',
   }
 })

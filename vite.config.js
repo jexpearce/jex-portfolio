db95527f025446c485867c0038b1e7e8c3e9f28f
Jex Pearce <jex@jajajeev.com>
2025-06-13 17:21:01 +0500

    b

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jex-portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/jex-portfolio/' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
